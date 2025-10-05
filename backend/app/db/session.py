cfcf52095bee7a802ecaf323408140b3bc356f3f
Dnay13 <u22240451@utp.edu.pe>
2025-10-05 14:10:32 -0500

    Avance Sprint 2
    Estructura y desarrollo del .env, db/base_class y session

---
+++ backend/app/db/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+# Motor para base de datos principal
+engine_main = create_engine(settings.MYSQL_MAIN_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine_main)
+
+# Motor para base de datos RADIUS
+engine_radius = create_engine(settings.MYSQL_RADIUS_URL)
+SessionRadius = sessionmaker(autocommit=False, autoflush=False, bind=engine_radius)
